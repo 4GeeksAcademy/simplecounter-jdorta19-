bc2e2fa82e2328e93b3fc36557fbcd38ab99f440
Jdorta19 <joeldorta19@gmail.com>
2024-12-11 13:18:00 +0000

    fixed erros jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,19 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 
-export const NavBar = ({ currentPage, setCurrentPage }) => {
+export const NavBar = ({ currentPage, pageSetter }) => {
   const linkMap = ["Landing", "Counter"];
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand
-            onClick={() => setCurrentPage("Landing")}
-          ></Navbar.Brand>
+          <Navbar.Brand onClick={() => pageSetter("Landing")}></Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               {linkMap.map((link) => {
                 return (
                   <Nav.Link
                     active={link === currentPage}
-                    onClick={() => setCurrentPage(link)}
+                    onClick={() => pageSetter(link)}
                   >
                     {link}
                   </Nav.Link>
